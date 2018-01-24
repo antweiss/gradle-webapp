6c49132e08fba3fe0217bcd791fe982ace367801
Anton Weiss <ant.weiss@gmail.com>
2018-01-24 16:44:40 +0200

    Change servlet name and message

--- src/main/java/link/otomato/DevOpsServlet.java
+++ src/main/java/link/otomato/DevOpsServlet.java
@@ -14,6 +14,6 @@ public class HelloWorldServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		resp.getOutputStream().write("Hello, Gradle.".getBytes());
+		resp.getOutputStream().write("Welcome to Devops".getBytes());
 	}
 }
